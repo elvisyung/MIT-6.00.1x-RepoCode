4205ec09aece3b5cbf4a32dab064ab7683c7cc7b
Elvis Yung <elvisckyung@gmail.com>
2022-02-17 10:04:15 +0800

    Add files via upload

---
+++ Unit2: Simple Programs/unit2-problemsets.py
@@ -0,0 +1,42 @@
+# Problem Sets
+
+# Problem Set 1 
+for i in range(12):
+    balance = balance - (balance * monthlyPaymentRate) + ((balance - (balance * monthlyPaymentRate)) * (annualInterestRate/12))
+print("Remaining balance:", round(balance, 2))
+
+# Problem Set 2 
+monthlyPaymentRate = 0
+init_balance = balance
+monthlyInterestRate = annualInterestRate/12
+
+while balance > 0:
+    for i in range(12):
+        balance = balance - monthlyPaymentRate + ((balance - monthlyPaymentRate) * monthlyInterestRate)
+    if balance > 0:
+        monthlyPaymentRate += 10
+        balance = init_balance
+    elif balance <= 0:
+        break
+print('Lowest Payment:', monthlyPaymentRate)
+
+# Problem Set 3
+
+init_balance = balance
+monthlyInterestRate = annualInterestRate/12
+lower = init_balance/12
+upper = (init_balance * (1 + monthlyInterestRate)**12)/12.0
+epsilon = 0.03
+
+while abs(balance) > epsilon:
+    monthlyPaymentRate = (upper + lower)/2
+    balance = init_balance
+    for i in range(12):
+        balance = balance - monthlyPaymentRate + ((balance - monthlyPaymentRate) * monthlyInterestRate)
+    if balance > epsilon:
+        lower = monthlyPaymentRate
+    elif balance < -epsilon:
+        upper = monthlyPaymentRate
+    else:
+        break
+print('Lowest Payment:', round(monthlyPaymentRate, 2))
